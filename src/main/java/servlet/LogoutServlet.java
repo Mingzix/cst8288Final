2f00b629a1560b2cbeee746f597d5686ffdbfd1a
ZhouZoeAlgonquin <159395951+ZhouZoeAlgonquin@users.noreply.github.com>
2024-04-07 22:25:19 -0400

    Update LogoutServlet.java
    Added JavaDoc and comments

--- src/main/java/servlet/LogoutServlet.java
+++ src/main/java/servlet/LogoutServlet.java
@@ -9,11 +9,20 @@ import java.io.IOException;
 
 /**
  * Servlet implementation class LogoutServlet
+ * @author Zoe Zhou
  */
 public class LogoutServlet extends HttpServlet {
+	/**
+	 * This function handles servlet requests made over HTTP GET method
+	 * @param req HttpServletRequest object that provides request information for HTTP servlets
+	 * @param resp HttpServletResponse object that provides HTTP-specific functionality in sending a response
+	 */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	// Invalidate current user's session
         req.getSession().invalidate();
+        
+        // Redirect user back to login page
         resp.sendRedirect("index.jsp");
     }
 }
